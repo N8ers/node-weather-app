bf094ca434a7cee6b9137a3382d6778394f1bdf2
N8ers <nathanraysheryak@gmail.com>
2018-08-29 11:37:51 -0400

    call back error handlers

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
-const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
 const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true //tells yargs to parce 'a' as a string
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv; //saves results to argv 
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-//HOW TO GET ADDRESS AND ENJECT IT INTO API
-//This encodes the address
-var encodedAddress = encodeURIComponent(argv.address);
-//If you look in the request below, under the url you will see a template string with enjected dynamic variable
-
-console.log(argv);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
-
-//don't know how to encode something, terminal <node>
-//<encodeURIComponent('what you want to encode')>
-//it gives you -> 'what%20you%20want%20to%20encode'
-//you can do the reverse with <decodeURIComponent('what%20you%20want%20to%20encode')
