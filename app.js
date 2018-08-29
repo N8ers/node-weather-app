4913663c0ca42a4c8ed893258726be5e532ed6d5
N8ers <nathanraysheryak@gmail.com>
2018-08-29 09:46:52 -0400

    Lat/lng test from google api

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const request = require('request');
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
-}, (error, responce, body) => {
-    console.log(body);
+}, (error, response, body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
